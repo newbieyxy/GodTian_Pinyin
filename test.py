764ce1627f32298e8e6635db0de8bc078a902cdb
newbieyxy <846232979@qq.com>
2019-01-09 09:54:25 +0000

    test mode--single word or phrase

--- test.py
+++ test.py
@@ -8,20 +8,24 @@ Offline evaluation of pinyin input method.
 """
 
 import re
+from pypinyin import lazy_pinyin, NORMAL
 from GodTian_Pinyin import GodTian_Pinyin
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--test-acc", type=str, default="top5", help="test mode: top5 or top10")
+parser.add_argument("--mode", type=str, default="single", help="test method: single(one word) or phrase(more than one word)")
 args = parser.parse_args()
 
-if __name__ == "__main__":
+def no_pinyin(ch):
+    return ' '
+
+# 测试集为多个句子，将其中的字一个一个提取出来进行测试
+def single_word_test(file_path, godtian):
     input_list = []
     expected_output_list = []
-    test_file = open('test_set.txt')
+    test_file = open(file_path, "rb")
     for line in test_file:
-        # 测试集是一行拼音一行文字
-        # 将拼音和文字分开
         is_char_low = re.match(r'[a-z\']', line[0]) 
         is_char_up = re.match(r'[A-Z\']', line[0])
         if is_char_low or is_char_up:
@@ -29,15 +33,14 @@ if __name__ == "__main__":
         else:
             expected_output_list.append(line)
      
-    
     # 将测试样本中的每句话的每个中文字对应的拼音提取，用单个中文字去测试
-    godtian = GodTian_Pinyin()
     test_num = 0
     correct_num = 0
     for test_line_num in range(len(input_list)):
+        # 整个句子的拼音
         hz_list = input_list[test_line_num].split(' ')
-        # print("测试句子",hz_list)
         for hz_idx in range(len(hz_list)):
+            # 一个字的拼音
             test_num += 1
             py = [] # 单个字的测试
             if "\n" in hz_list[hz_idx]:
@@ -50,8 +53,6 @@ if __name__ == "__main__":
                 hz = godtian.cache["".join(py)]
             
             # print("hz ",hz)
-            res_py_list = []
-            res_score_list = []
             if args.test_acc == "top5":
                 for i in range(5):
                     if i > len(hz)-1:
@@ -67,6 +68,70 @@ if __name__ == "__main__":
             else:
                 raise Exception
         
-    print("测试模式{}：完成{}个句子的测试，准确率为{}".format(args.test_acc, len(input_list), float(correct_num/test_num)))       
+    return float(correct_num/test_num), test_num
+
+# 测试集为多个词组，对整个词组进行测试
+def combination_test(file_path, godtian):
+    fopen = open(file_path, "rb")
+    py_data = []
+    hz_data = []
+    i = 0
+    for line in fopen:
+        line = line.decode("utf-8")
+        line = line.split(" ")
+        if len(line)>=1:
+            word = line[0].split("\t")
+            word = word[0]
+            py_list = lazy_pinyin(word, style=NORMAL, errors=no_pinyin) # 将词组转换为拼音
+            if py_list[0] == " ":
+                continue # 文件开头，第一个词的前面会有空格
+            py_data.append(py_list)
+            hz_data.append(word)
+                       
+        i += 1
+        if i>=1000:
+            print("hz_data",hz_data)
+            print("py_data",py_data)
+            break
+    
+    # 对整个词组进行测试
+    test_num = 0
+    correct_num = 0
+    for phrase_idx  in range(len(py_data)):
+        test_num += 1
+        py = "".join(py_data[phrase_idx])
+        if py not in godtian.cache:
+            hz, two_part = godtian.handle_current_input(py, 15, 15)
+            godtian.cache[py] = hz
+        else:
+            hz = godtian.cache[py]
+
+        if args.test_acc == "top5":
+            for i in range(5):
+                if i > len(hz)-1:
+                    break
+                if "".join(hz[i].path) == hz_data[phrase_idx]:
+                    correct_num += 1
+        elif args.test_acc == "top10":
+            for i in range(10):
+                if i > len(hz)-1:
+                    break
+                if "".join(hz[i].path) == hz_data[phrase_idx]:
+                    correct_num += 1
+        else:
+            raise Exception
+    
+    return float(correct_num/test_num), test_num
 
 
+if __name__ == "__main__":
+    godtian = GodTian_Pinyin()
+    if args.mode == "single":
+        file_path = "./test_dataset/sentence.txt"
+        correct_rate, test_num = single_word_test(file_path, godtian)
+    elif args.mode == "phrase":
+        file_path = "./test_dataset/sogou_dic_new.txt"
+        correct_rate, test_num = combination_test(file_path, godtian)
+    else:
+        raise Exception
+    print("完成{}个样本的测试，测试模式{}，测试精度{}，准确率为{}".format(test_num, args.mode, args.test_acc, correct_rate))
