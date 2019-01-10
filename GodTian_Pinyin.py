95463ce9b6c346342141d259cb38d6b91bd28d8d
newbieyxy <846232979@qq.com>
2019-01-10 03:48:53 +0000

    merge viterbi and newviterbi

--- GodTian_Pinyin.py
+++ GodTian_Pinyin.py
@@ -43,7 +43,7 @@ def emit_a_b_many(emit,hanyu,pinyin):
     if hanyu in emit:
         judge = 0
         max_pinyin = -9999
-        max_i = -1
+        # max_i = -1 # useless
         for val in emit[hanyu]:
             for i in range(0,len(val)):
                 if pinyin == val[:i]:
@@ -56,14 +56,14 @@ def emit_a_b_many(emit,hanyu,pinyin):
 
 def serch_in_dict(pyl,dict):
     res = ""
-    ii = 15
+    # ii = 15 # useless
     for i in pyl:
         if i!=" ":
             res += i
     res += "  "
     if res in dict:
         list =  PrioritySet(15)
-        s = sorted(dict[res].iteritems(), key=lambda d: d[1], reverse=True)
+        s = sorted(dict[res].items(), key=lambda d: d[1], reverse=True) # dict.itertiems(python2) --> dict.items(python3)
         mm = 0
         for j in s:
             list1 = []
@@ -99,50 +99,93 @@ class GodTian_Pinyin(object):
                     self.memo[key1][key2] = memo[key1][key2]
         self.sp = sp.SplitPinyin()
 
-    def newviterbi(self, pylist, top=15):
-        V = [{} for _ in range(2)]
-        t = 0
-        idx = 0
-        cur_obs = pylist[t]  #
-
-        topp =100
-
-        # use trie tree to get the prefix
-        prefix_ans = {}
-        self.pt.get_totalwords_of_prefix(self.pt.root,pylist[0], prefix_ans)
-        sorted_pf_ans = sorted(prefix_ans.items(), key=lambda x: x[1], reverse=True)
-        words = [hz_freq[0] for hz_freq in sorted_pf_ans[:topp]]
-        cur_cand_states = words  # 可能状态
-        for i in cur_cand_states:
-            print(i)
-
-        prepyseq = "".join(pylist[:-1])
-        pylislen = len(pylist)
+    # 预判断和计算Viterbi
+    def use_viterbi(self, pylist, top=15, topp =100, words=[], mode=None):
+        # pylist:split of pinyin -- [py1,py2,...]
+        use_top = top
+        use_topp = topp #只考虑前topp的结果
+        use_words = words
+        use_mode = mode
+        print("use_viterbi, mode {}".format(mode))
+        # V[idx]的结构是
+        # {'汉字'：[<score, path>,<score, path>,...],
+        #  '汉字'：[<score, path>,<score, path>,...],
+        #  ...}; 
+        V = [{} for _ in range(2)] # 两个字典，一个用来存当前的状态，另一个用来记录前一个状态
         START = 1
-
-        for state in cur_cand_states:
-            tao = Pi_state(self.Pi, state) + emit_a_b_many(self.emit, state, cur_obs)
-            _path = [state]
-            V[0].setdefault(state, PrioritySet(top))
-            V[0][state] = PrioritySet(top)
-            V[0][state].put(tao, _path)
-
-        for t in range(START, pylislen):
-            cur_obs = pylist[t]
-            print ("---------------")
-            print (pylist,t,pylist[t])
-            idx = t % 2
-            V[idx] = {}
-            prev_states = cur_cand_states
-
+        pylist_len = len(pylist)
+        
+        # 处理第一个拼音（及对应的状态）
+        cur_obs = pylist[0]
+        if mode == "many_part":
+            # self.pt.get_totalwords_of_prefix(self.pt.root, pylist[0], prefix_ans)
             prefix_ans = {}
             self.pt.get_totalwords_of_prefix(self.pt.root, cur_obs, prefix_ans)
             sorted_pf_ans = sorted(prefix_ans.items(), key=lambda x: x[1], reverse=True)
             words = [hz_freq[0] for hz_freq in sorted_pf_ans[:topp]]
-            cur_cand_states = words  # 可能状态
-            for i in cur_cand_states:
-                print(i)
+            cur_cand_states = words
+            for state in cur_cand_states:
+                tao = Pi_state(self.Pi, state) + emit_a_b_many(self.emit, state, cur_obs)
+                _path = [state]
+                V[0].setdefault(state, PrioritySet(top))  # {...,'汉字':[]}
+                V[0][state] = PrioritySet(top) # {...,'汉字':[]}
+                V[0][state].put(tao, _path)# {...,'汉字':[<score, path>]}
+        elif mode == "two_part":
+            pre_pyseq = "".join(pylist[:-1]) # 将前面完整的拼音部分组合到一起            
+            # 如果pre_pyseq在memo中，直接查询，不用重新计算，最后一个拼音pylist[-1]才使用Viterbi
+            # 如果pre_pyseq不在memo中，从第一个状态开始计算Viterbi
+            if pre_pyseq in self.memo:
+                # 将memo中的pre_pyseq对应的可能状态都找出来
+                cur_cand_states = []
+                for state in self.memo[pre_pyseq]:
+                    cur_cand_states.append(state)
+                    V[pylist_len%2][state] = self.memo[pre_pyseq][state] # 因为最后一个时刻为pylist_len-1，因此前面的都存在V[(pylist_len)%2]中
+                START = pylist_len - 1
+            else:
+                cur_cand_states = self.py2ch[cur_obs]
+                for state in cur_cand_states:
+                    tao = Pi_state(self.Pi, state) + emit_a_b_many(self.emit, state, cur_obs)
+                    _path = [state]
+                    V[0].setdefault(state, PrioritySet(top))
+                    V[0][state] = PrioritySet(top)
+                    V[0][state].put(tao, _path)
+                
+        res = self.viterbi(pylist, START, V, cur_cand_states, use_top, use_topp, use_words, use_mode)
+        
+        return res
 
+    # Viterbi算法
+    def viterbi(self, pylist, START, V, cur_cand_states, top=15, topp=100, words=[], mode=None):
+        # 再处理pylist第二个及后面的拼音
+        # 在trie树中找到pylist[0]即第一个拼音分割py1的所有以它为前缀的词组prefix_ans
+        # 并对prefix_ans中的结果进行排序
+        # 将前topp个结果赋给cur_cand_states作为可能状态（即可能的汉字）
+        # 状态即为汉字
+        pylist_len = len(pylist)
+        for t in range(START, pylist_len): # 状态的时刻
+            cur_obs = pylist[t]
+            idx = t % 2 # 存储V的索引
+            # 实际上是一个递推的过程，因此只用考虑前一个状态，因为前一个状态的path已经考虑了再前面的状态
+            V[idx] = {}
+            prefix_ans = {}             
+            # 更新上一时刻记录的状态    
+            prev_states = cur_cand_states # 上一个拼音py的可能状态
+            
+            # 更新当前状态，即当前拼音对应的可能状态
+            if mode == "many_part":
+                # 同样地，在trie树中找到pylist[t]即当前py的所有以它为前缀的词组prefix_ans
+                # 排序，并取前topp个结果，作为可能的状态（即可能的汉字）
+                self.pt.get_totalwords_of_prefix(self.pt.root, cur_obs, prefix_ans)
+                sorted_pf_ans = sorted(prefix_ans.items(), key=lambda x: x[1], reverse=True)
+                words = [hz_freq[0] for hz_freq in sorted_pf_ans[:topp]]
+                cur_cand_states = words
+            elif mode == "two_part":
+                if not words:
+                    cur_cand_states = self.py2ch[cur_obs]
+                else:
+                    cur_cand_states = words
+
+            # 当前记录V[idx][state]；上一个状态记录V[(idx+1)%2][prev]
             for state in cur_cand_states:  # 此时状态
                 V[idx].setdefault(state, PrioritySet(top))
                 for prev in prev_states:  # 前一个状态
@@ -151,72 +194,16 @@ class GodTian_Pinyin(object):
                         new_tao = tao + cand.score
                         _p = cand.path + [state]
                         V[idx][state].put(new_tao, _p)
+                # print("V[{}] {}".format(idx, V[idx]))
+                
+        # 只用看最后的V[idx]，相当于包含了前面所有的路径
         results = PrioritySet(top)
         for last_state in V[idx]:
+            if mode == "two_part":
+                self.memo["".join(pylist)][last_state] = V[idx][last_state] # 将整个拼音串的所有最后状态(包括score和path)存入memo中
             for item in V[idx][last_state]:
                 results.put(item.score, item.path)
         results = [item for item in results]
-        return sorted(results, key=lambda x: x.score, reverse=True)
-
-    def viterbi(self, pylist, top=15, words=[]):
-
-        V = [{} for _ in range(2)]
-        t = 0
-        idx = 0
-        cur_obs = pylist[t]  # 现在的观察
-        cur_cand_states = self.py2ch[cur_obs]  # 可能状态
-        prepyseq = "".join(pylist[:-1])
-        pylislen = len(pylist)
-        START = 1
-        TAG = 0
-        if prepyseq in self.memo:
-            TAG = 1
-            start = time.time()
-            T = pylislen-1   # Last one's index
-            cur_cand_states = []
-            for state in self.memo[prepyseq]:
-                cur_cand_states.append(state)
-                V[pylislen % 2][state] = self.memo[prepyseq][state]
-            START = T
-            end = time.time()
-            print("READ MEMORY COST {}".format(end-start))
-        else:
-            for state in cur_cand_states:
-                tao = Pi_state(self.Pi, state) + emit_a_b(self.emit, state, cur_obs)
-                _path = [state]
-                V[0].setdefault(state, PrioritySet(top))
-                V[0][state] = PrioritySet(top)
-                V[0][state].put(tao, _path)
-        # Iteration T > 0
-        start = time.time()
-        for t in range(START, pylislen):
-            cur_obs = pylist[t]
-            idx = t % 2
-            V[idx] = {}
-            prev_states = cur_cand_states
-            if not words:
-                cur_cand_states = self.py2ch[cur_obs]
-            else:
-                cur_cand_states = words
-            for state in cur_cand_states:  # 此时状态
-                V[idx].setdefault(state, PrioritySet(top))
-                for prev in prev_states:   # 前一个状态
-                    for cand in V[(idx+1) % 2][prev]:  # 前一个状态为prev, cand的概率
-                        tao = trans_a_b(self.trans, prev, state) + emit_a_b(self.emit, state, cur_obs)
-                        new_tao = tao + cand.score
-                        _p = cand.path + [state]
-                        V[idx][state].put(new_tao, _p)
-        end = time.time()
-        print("RUN VITERBI COST： {}".format(end-start))
-        start = time.time()
-        results = PrioritySet(top)
-        for last_state in V[idx]:
-            self.memo["".join(pylist)][last_state] = V[idx][last_state]  # 记住拼音串所有最后状态的Priority集
-            for item in V[idx][last_state]:
-                results.put(item.score, item.path)
-        results = [item for item in results]
-        end = time.time()
-        print("LAST PROCESSING: {}".format(end-start))
         return sorted(results, key=lambda x: x.score, reverse=True)
 
     def save_memo_and_cache(self):
@@ -228,15 +215,15 @@ class GodTian_Pinyin(object):
         input = input.lower()
         if self.pat.findall(input):   # 全数字，直接返回
             return input
-        pyl, two_part,may_parts = self.sp.split_pinyin(input)
-        print(pyl, two_part,may_parts)
-        if two_part == True and may_parts == False:
+        pyl, two_part,many_parts = self.sp.split_pinyin(input)
+        print(pyl, two_part, many_parts)
+        if two_part == True and many_parts == False: # 只有最后一组拼音是不完整的
             prefix_ans = {}
             start = time.time()
             self.pt.get_totalwords_of_prefix(self.pt.root, pyl[-1], prefix_ans)
             sorted_pf_ans = sorted(prefix_ans.items(), key=lambda x: x[1], reverse=True)
             end = time.time()
-            print("GET PREFIX COST: {}".format(end-start))
+            # print("GET PREFIX COST: {}".format(end-start))
             words = [hz_freq[0] for hz_freq in sorted_pf_ans[:topp]]
             # -------------------------
             best_viterbi_ans = []
@@ -245,24 +232,24 @@ class GodTian_Pinyin(object):
             start = time.time()
             for _, py in enumerate(pinyins):
                 pyl[-1] = py
-                viterbi_ans = self.viterbi(pyl, topv, [words[_]])  # self.momo["".join(pyl[:-1]][state...] =
+                # viterbi_ans = self.viterbi(pyl, topv, [words[_]])  # self.memo["".join(pyl[:-1]][state...] =
+                viterbi_ans = self.use_viterbi(pyl, topv, [words[_]], mode="two_part")
             end = time.time()
-            print("VITERBI COST: {}".format(end-start))
+            # print("VITERBI COST: {}".format(end-start))
             best_viterbi_ans.extend(viterbi_ans)
             return best_viterbi_ans, two_part
-        elif may_parts:
+        elif many_parts: # 中间的拼音也是不完整的
             new_viterbi_ans = serch_in_dict(pyl,self.dict)
-            print (new_viterbi_ans)
+            # print ("new_viterbi_and ",new_viterbi_ans)
             if new_viterbi_ans ==[]:
-               new_viterbi_ans = self.newviterbi(pyl, topv)
+               # new_viterbi_ans = self.newviterbi(pyl, topv, mode="many_part")
+               new_viterbi_ans = self.use_viterbi(pyl, topv, mode="many_part")
             return new_viterbi_ans,two_part
-        else:
-            viterbi_ans = self.viterbi(pyl, topv, [])
-            print (viterbi_ans)
+        else: # 所有拼音都是完整的
+            # viterbi_ans = self.viterbi(pyl, topv, [])
+            viterbi_ans = self.use_viterbi(pyl, topv, mode="two_part")
+            # print ("viterbi_ans ",viterbi_ans)
             return viterbi_ans, two_part
-
-
-
 
 # if __name__ == '__main__':
 #
